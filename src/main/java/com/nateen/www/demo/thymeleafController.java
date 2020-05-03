b9cf774252e701c59033a5c309c29142dae90451
lyujaejun <nateen7248@naver.com>
2020-05-03 22:48:25 +0900

    thymeleaf controller, view 파일 생성

--- src/main/java/com/nateen/www/demo/thymeleafController.java
+++ src/main/java/com/nateen/www/demo/thymeleafController.java
@@ -1,13 +1,17 @@
 package com.nateen.www.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class thymeleafController {
 
     @GetMapping(value = "home")
-    public String thymeleafView(){
+    public String thymeleafView(Model model){
+
+        model.addAttribute("data","hello nateen");
+
         return "/home/home";
     }
 
